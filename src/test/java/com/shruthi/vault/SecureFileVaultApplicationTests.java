e54457a9ee35e30cd1976e0e1c1ffc60ecf659d3
Shruthi Eluri <elurishruthi1999@gmail.com>
2025-05-08 00:44:36 +0100

    Initial commit - Spring Boot setup

---
+++ src/test/java/com/shruthi/vault/SecureFileVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruthi.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureFileVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
